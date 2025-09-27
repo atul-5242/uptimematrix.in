426b0ffdd58fd9255296d90aab1c6426e36250cd
Atul Maurya <atul.fzdlko2001@gmail.com>
2025-09-27 16:40:56 +0530

    SIgnup testing error 2.

--- apps/web/app/api/auth/signup/route.ts
+++ apps/web/app/api/auth/signup/route.ts
@@ -22,6 +22,6 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(data, { status: response.status });
     }catch (error) {
       console.error("Sign up error:", error);
-      return NextResponse.json({ message: "Failed to sign up" }, { status: 500 });  
+      return NextResponse.json({ message: "Failed to sign up",baseurlCheck: process.env.NEXT_PUBLIC_API_URL }, { status: 500 });  
     }
 }
